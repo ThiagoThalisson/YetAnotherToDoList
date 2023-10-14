29d63860a9f2530ce178de39bcb11b6f543e1007
ThiagoThalisson <atomic-feast@proton.me>
2023-10-14 08:18:28 -0300

    Added all project file to remote repo

---
+++ src/main/java/br/com/atomicfeast/YetAnotherToDoList/YetAnotherToDoListApplication.java
@@ -0,0 +1,13 @@
+package br.com.atomicfeast.YetAnotherToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetAnotherToDoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/atomicfeast/YetAnotherToDoList/YetAnotherToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atomicfeast.YetAnotherToDoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetAnotherToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
